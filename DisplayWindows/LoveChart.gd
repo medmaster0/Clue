3a6b51992b7bff682cfd782c53fb0ee246144401
medmaster0 <antbaca9@gmail.com>
2020-01-20 09:41:28 -0700

    Love chart draws lines for connections

--- DisplayWindows/LoveChart.gd
+++ DisplayWindows/LoveChart.gd
@@ -6,14 +6,35 @@ export (PackedScene) var Creature
 # var a = 2
 # var b = "text"
 
+var placed_cres = [] #creatures taht have been placed
+var min_distance = 16*4.5
+var my_chart_data #will hold the chart data
+var line_color #the color of the line connections
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	randomize()
+	line_color = MedAlgo.generate_pink()
 	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+#called every time it's drawn
+func _draw():
+	#For each of the love connections.. draw a line
+	for i in range(my_chart_data["num_creatures"]):
+		for c in my_chart_data["web_of_love"][i]:
+			# so i is lover index
+			# so c is target index
+			var ipos = placed_cres[i].position + Vector2(8,8) #Add an offset so we hit the middle of the sprite...
+			var cpos = placed_cres[c].position + Vector2(8,8) #Add an offset so we hit the middle of the sprite...
+			#draw the line
+			draw_line(ipos, cpos, line_color, 4)
+	
 
 #Function that will load up (and draw) the input given love_chart data
 #	var chart_data = {
@@ -23,8 +44,8 @@ func _ready():
 # Basically draw the creatures scattered randomly (but spaced out)
 func load_data(chart_data,map_creatures):
 	
-	var placed_cres = [] #creatures taht have been placed
-	var min_distance = 16*4.5
+	#store the chart data
+	my_chart_data = chart_data
 	
 	#Draw each of the creatures in a random position
 	for i in range(chart_data["num_creatures"]):
